4b5bcc5f85d0032d458f16c01c56da9ed45dc1d8
anycodes <jiangyu.ly@alibaba-inc.com>
2020-09-19 00:28:15 +0800

    fix readme

--- src/bootstrap.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const DEFAULTBOOTSTRAP = "#!/usr/bin/env bash\n\nexport PORT={port}\n{start}";
-export declare const DEFAULTPORT = 9000;
-export declare const DEFAULTSTART = "npx next start --port $PORT";
-//# sourceMappingURL=bootstrap.d.ts.map
--- src/bootstrap.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DEFAULTSTART = exports.DEFAULTPORT = exports.DEFAULTBOOTSTRAP = void 0;
-exports.DEFAULTBOOTSTRAP = "#!/usr/bin/env bash\n\nexport PORT={port}\n{start}";
-exports.DEFAULTPORT = 9000;
-exports.DEFAULTSTART = 'npx next start --port $PORT';
-//# sourceMappingURL=bootstrap.js.map
